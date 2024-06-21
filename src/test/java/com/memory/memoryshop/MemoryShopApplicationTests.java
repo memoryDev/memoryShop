16a011e1df99693cfa04bae46c220c93b1670254
memoryDev <memorydev2024@gmail.com>
2024-06-21 17:41:51 +0900

    Initial commit

---
+++ src/test/java/com/memory/memoryshop/MemoryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memory.memoryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
